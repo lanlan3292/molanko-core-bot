401aabc68b70760e1e2150a1d5c0ef8dab972275
lanlan3292 <149509569+lanlan3292@users.noreply.github.com>
2026-09-05 07:52:23 +0800

    测试

---
+++ commands/version/version.py
@@ -0,0 +1,13 @@
+import os
+from dataclasses import dataclass
+
+
+@dataclass
+class VersionInfo:
+    version: str
+
+
+class VersionService:
+    def get_version(self) -> VersionInfo:
+        version = os.getenv("MOLANKO_BOT_VERSION", "unknown")
+        return VersionInfo(version=version)
